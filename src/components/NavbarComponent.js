dffedd644f87ac223f920c359e33a8f129e65458
nb341 <narindrab4@gmail.com>
2021-02-13 17:04:53 -0400

    fixed linked not changing colour

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -18,19 +18,19 @@ class Navbar extends Component{
                 <nav className="nav-top">
                     <ul className="nav-items">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/"><HomeOutlinedIcon style={{fontSize: 40}}/></Link>
+                            <Link className="nav-link" to="/"><HomeOutlinedIcon className="nav-icon" style={{fontSize: 40}}/></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/aboutme"><PersonOutlineOutlinedIcon style={{fontSize: 40}}/></Link>
+                            <Link className="nav-link" to="/aboutme"><PersonOutlineOutlinedIcon className="nav-icon" style={{fontSize: 40}}/></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/skills"><SettingsOutlinedIcon style={{fontSize: 40}}/></Link>
+                            <Link className="nav-link" to="/skills"><SettingsOutlinedIcon className="nav-icon" style={{fontSize: 40}}/></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/portfolio"><BusinessCenterOutlinedIcon style={{fontSize: 40}}/></Link>
+                            <Link className="nav-link" to="/portfolio"><BusinessCenterOutlinedIcon className="nav-icon" style={{fontSize: 40}}/></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contact"><EmailOutlinedIcon style={{fontSize: 40}}/></Link>
+                            <Link className="nav-link" to="/contact"><EmailOutlinedIcon className="nav-icon" style={{fontSize: 40}}/></Link>
                         </li>
                     </ul>
                 </nav>
@@ -43,6 +43,7 @@ class Navbar extends Component{
                             className="nav-link" 
                             href="https://github.com/nb341/" 
                             target="_blank"><GitHubIcon
+                            className="nav-icon"
                             style={{fontSize: 40}}/>
                             </a>
                         </li>
@@ -53,7 +54,7 @@ class Navbar extends Component{
                         href="https://www.linkedin.com/in/narindra-balkissoon/" 
                         target="_blank"
                         >
-                            <LinkedInIcon style={{fontSize: 40}}/>
+                            <LinkedInIcon className="nav-icon" style={{fontSize: 40}}/>
                         </a>
                         </li>
                         <li className="nav-item">
@@ -63,7 +64,7 @@ class Navbar extends Component{
                             target="_blank" 
                             href="https://codepen.io/nb341/"
                             >
-                                <CodeIcon style={{fontSize: 40}}/>
+                                <CodeIcon className="nav-icon" style={{fontSize: 40}}/>
                             </a>
                         </li>
                     </ul>
